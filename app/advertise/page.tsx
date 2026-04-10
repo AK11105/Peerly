369400b67d673e4cb1586eb4a3d2cf746fdb5a6b
rish007 <rishabh.b1910@gmail.com>
2026-04-10 19:39:04 +0530

    add dummy advertise page

--- app/advertise/page.tsx
+++ app/advertise/page.tsx
@@ -4,7 +4,7 @@ export default function AdvertisePage() {
       <div className="text-center space-y-4">
         <h1 className="text-4xl font-bold text-foreground">Advertise on Peerly</h1>
         <p className="text-muted-foreground max-w-xl">
-          Advertise your productivity tools in return for providing exclusive subscriptions redeemable through lumens ⭐
+          Advertise your productivity tools in return for providing <br /><span className="font-bold text-foreground/90  text-lg">exclusive subscriptions</span> <br/>redeemable through LUMENS ⭐
         </p>
       </div>
     </div>
